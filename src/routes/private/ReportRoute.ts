b03e9e8fc14fdb4c4612d4206883ba32b0183325
chris <christoperjoel1@gmail.com>
2026-05-27 14:33:08 +0700

    change validate report http (/validate/:id) from post to patch

--- src/routes/private/ReportRoute.ts
+++ src/routes/private/ReportRoute.ts
@@ -10,6 +10,6 @@ router.post("/", authMiddleware, reportController.createReport);
 //! Admin side
 router.get("/staff", authMiddleware, reportController.showAllReportByDivision)
 router.get("/staff/:id", authMiddleware, reportController.showDetailReportStaff)
-router.post("/validate/:id", authMiddleware, reportController.validateReport)
+router.patch("/validate/:id", authMiddleware, reportController.validateReport)
 
 export default router;
